aa084735fc6bbe898797c4ed9b8037bca1fe6f62
wneill33 <west.neill1@gmail.com>
2024-04-30 11:00:48 -0500

    added levels to ui

--- src/base/Steps/Step4/StepFour.jsx
+++ src/base/Steps/Step4/StepFour.jsx
@@ -1,8 +1,19 @@
 import "./stepFour.css";
 const StepFour = () => {
   return (
-    <div className="stepFourMainWrapper">
-      <h1>Step Four</h1>
+    <div className="stepFourWrapper">
+      <h1>Level 4: The Canyon Hideout</h1>
+      <div className="stepFourInstructionswrapper">
+        <div className="stepFourone">
+          Following a lead, you embark on a treacherous journey into the
+          canyons, where the outlaw gang is rumored to have a hideout. Your
+          challenge: navigate the rugged terrain while evading traps and
+          ambushes set by the outlaws. (When setting it up, you will use ls -a
+          to find their hidden hideout (folder) but they will have already left
+          when you get there.)
+        </div>
+        <div className="stepFourtwo">Hint: dir, cat, ls -a, man</div>
+      </div>
     </div>
   );
 };
--- src/base/Steps/Step5/StepFive.jsx
+++ src/base/Steps/Step5/StepFive.jsx
@@ -1,8 +1,19 @@
 import "./stepFive.css";
 const StepFive = () => {
   return (
-    <div className="stepFiveMainWrapper">
-      <h1>Step Five</h1>
+    <div className="stepFiveWrapper">
+      <h1>Level 5: The Ghost Town</h1>
+      <div className="stepFiveInstructionswrapper">
+        <div className="stepFiveone">
+          With the outlaws behind bars, you set out to explore a nearby ghost
+          town rumored to hold hidden treasures. But beware, for the town is
+          said to be haunted by vengeful spirits seeking to protect their
+          secrets.
+        </div>
+        <div className="stepFivetwo">
+          Hint: dir, ls, cat, man, find, rmdir, sudo
+        </div>
+      </div>
     </div>
   );
 };
